d68eb325b4ee8004da0395ced1f05ada99d936e5
erwan boehm <erwan.boehm@gmail.com>
2018-05-17 18:42:26 +0200

    adding render

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -19,11 +19,11 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    for (int j = 0; j< 4; j++) {
+    for (int j = 0; j< 40; j+=10) {
         for (int i = 0; i < 1000; i++) {
-            int scale = 3000;
-            ofSetColor(ofColor((ofNoise(r*i/scale)*255), (ofNoise(g*i/scale)*255), (ofNoise(b*i/scale)*255)));
-            ofDrawCircle(ofNoise(b*j*i/scale)*ofGetWidth(),ofNoise(g*j*i/scale)*ofGetHeight(),ofNoise(b*i/scale)*100);
+            int scale = 10000;
+            ofSetColor(ofColor((ofNoise(r*i/scale)*2550), (ofNoise(g*i/scale)*2550), (ofNoise(b*i/scale)*2550)));
+            ofDrawCircle(ofNoise(i*j+b*i/scale)*ofGetWidth(),ofNoise(i*j+g*i/scale)*ofGetHeight(),ofNoise(b*i/scale)*50);
         }
     }
 }
