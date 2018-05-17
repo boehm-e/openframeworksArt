f2356df251c952ea06e71ee343864a770f6bcbe5
erwan boehm <erwan.boehm@gmail.com>
2018-05-17 22:04:02 +0200

    adding demo

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -20,15 +20,16 @@ void ofApp::update(){
 void ofApp::draw(){
     ofTranslate(ofPoint(ofGetWidth()/2, ofGetHeight()/2));
 
-    float radius= ofGetWidth() / 4;
-    int numPoints= (int)ofMap(ofNoise(r+g+b), 0, 1, 0, 500);
-    float angle=TWO_PI/(float)numPoints;
+    float radius= ofNoise(b) * 4 * ofGetWidth() / 4;
+    int numPoints = ofMap(ofNoise(b), 0, 1, 0, 1000);
+    float angle=TWO_PI/(float)numPoints*15;
 
     for(int i=0;i<numPoints;i++) {
         ofSetColor(ofNoise(r+i) * 255, ofNoise(g+i) * 255, ofNoise(b+i) * 255);
-        ofDrawCircle(radius*sin(angle*i),radius*cos(angle*i), 5);
-//        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i+1), radius*cos(angle*i+1)));
-        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i-i%numPoints/2), radius*cos(angle*i-i%numPoints/2)));
+
+        ofDrawCircle(cos(i)*(radius-i)*ofNoise(b)*sin(angle*i),cos(i)*(radius-i)*ofNoise(b)*cos(angle*i), 5);
+
+        //        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i-i%numPoints/2), radius*cos(angle*i-i%numPoints/2)));
     }
 }
 
