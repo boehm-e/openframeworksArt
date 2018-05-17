47e9ac59d6b8ab8aa1ac47c7d1d2014a1bf3005d
erwan boehm <erwan.boehm@gmail.com>
2018-05-17 20:25:00 +0200

    adding demo

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -27,8 +27,8 @@ void ofApp::draw(){
     for(int i=0;i<numPoints;i++) {
         ofSetColor(ofNoise(r+i) * 255, ofNoise(g+i) * 255, ofNoise(b+i) * 255);
         ofDrawCircle(radius*sin(angle*i),radius*cos(angle*i), 5);
-//        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i+1), radius*cos(angle*i+1)));
-        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i-i%numPoints/2), radius*cos(angle*i-i%numPoints/2)));
+        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i+1), radius*cos(angle*i+1)));
+        ofDrawLine(ofPoint(radius*sin(angle*i), radius*sin(angle*i)), ofPoint(radius*sin(angle*i-1), radius*cos(angle*i-1)));
     }
 }
 
