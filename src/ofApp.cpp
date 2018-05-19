c0e4deed1f2115265522b1ab548baa41215dd34e
erwan boehm <erwan.boehm@gmail.com>
2018-05-19 02:12:25 +0200

    some improvments to double_pendule

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,35 +1,39 @@
 #include "ofApp.h"
+#include "of3dGraphics.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    r = ofRandom(5);
-    g = ofRandom(5);
-    b = ofRandom(5);
+    r = 0;
+    g = 1;
+    b = 2;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     screen.grabScreen(0,0,ofGetWidth(), ofGetHeight());
-    float scale = 0.2;
+    float scale = 2;
     r += 0.01*scale;
-    g += 0.02*scale;
+    g += 0.012*scale;
     b += 0.015*scale;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofTranslate(ofPoint(ofGetWidth()/2, ofGetHeight()/2));
+    float radiusX = ofGetWidth()/3;
+    float radiusY = ofGetWidth() / 3;
+    float angle1 = ofMap(ofNoise(r), 0, 1, 5*PI/6, PI/6);
+    float angle2 = ofMap(ofNoise((g+r)/2), 0, 1, PI, 0);
 
+    ofPoint firstPendule = ofPoint(ofGetWidth()/2, 10);
+    ofPoint secondPendule = ofPoint(radiusX*cos(angle1)+firstPendule.x, radiusY*sin(angle1)+firstPendule.y);
+    ofPoint thirdPendule = ofPoint(radiusX*cos(angle2)+secondPendule.x, radiusY*sin(angle2)+secondPendule.y);
+    ofSetColor(ofNoise(r)*255, ofNoise(g)*255, ofNoise(b)*255);
+    ofDrawCircle(firstPendule, 5);
+    ofDrawLine(firstPendule, secondPendule);
+    ofDrawCircle(secondPendule, 5);
+    ofDrawLine(secondPendule, thirdPendule);
+    ofDrawCircle(thirdPendule, 5);
 
-    int numPoints = 50;
-    for(int x=-numPoints;x<numPoints;x++) {
-        for(int y=-numPoints;y<numPoints;y++) {
-            int _x = (int) ofMap(x, 0, numPoints, 0, ofGetWidth());
-            int _y = (int) ofMap(y, 0, numPoints, 0, ofGetHeight());
-            ofSetColor(ofNoise(r+x+y) * 255, ofNoise(g+x+y) * 255, ofNoise(b+x+y) * 255);
-            ofDrawCircle(ofNoise(cos(x)+sin(y)+g)*_x, ofNoise(sin(x)+cos(y)+b)*_y, 5);
-        }
-    }
 }
 
 //--------------------------------------------------------------
